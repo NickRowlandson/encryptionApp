65595b970d884c9b02df80ee62cdb792747b81f7
NickRowlandson <nicholasrowlandson@gmail.com>
2017-05-29 20:35:33 -0400

    initial setup complete

---
+++ app.js
@@ -0,0 +1,17 @@
+/* app.js */
+
+// require and instantiate express
+const app = require('express')()
+
+// set the view engine to ejs
+app.set('view engine', 'ejs')
+
+// blog home page
+app.get('/', (req, res) => {
+  // render `home.ejs` with the list of posts
+  res.render('home')
+})
+
+app.listen(8080)
+
+console.log('listening on port 8080')
