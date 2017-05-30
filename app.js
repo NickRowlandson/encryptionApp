89c1359927cbde2658f6045f62fe5401ee07653c
NickRowlandson <nicholasrowlandson@gmail.com>
2017-05-30 13:46:53 -0400

    completed decrypt functionality

--- app.js
+++ app.js
@@ -1,26 +1,35 @@
-/* app.js */
+/* app.js
+Author: Nicholas Rowlandson
+Description: Vignere Cipher - Assignment #3 COMP3026
+Date: May 30th 2017
+*/
 
-// require and instantiate express
 var express = require('express'),
   bodyParser = require('body-parser');
 
 var app = express();
 
+var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h",
+  "i", "j", "k", "l", "m", "n", "o", "p", "q",
+  "r", "s", "t", "u", "v", "w", "x", "y", "z"
+];
+
+var decryptedMessage = "";
+var encryptedMessage = "";
+
 app.use(bodyParser());
 // set the view engine to ejs
 app.set('view engine', 'ejs')
 
-// blog home page
 app.get('/', (req, res) => {
   // render `home.ejs` with the list of posts
+  res.locals.decryptedMessage = '';
+  res.locals.encryptedMessage = '';
   res.render('home');
 })
 
+// Read input from user and encrypt message using provided key and rot type
 app.post('/encrypt', function(req, res, next) {
-  var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h",
-    "i", "j", "k", "l", "m", "n", "o", "p", "q",
-    "r", "s", "t", "u", "v", "w", "x", "y", "z"
-  ];
 
   var paired = new Array();
   var keyLength = req.body.encryptKey.length;
@@ -35,9 +44,7 @@ app.post('/encrypt', function(req, res, next) {
     });
     count++;
   }
-console.log(paired);
 
-  var encryptedMessage = "";
   for (var i = 0; i < paired.length; i++) {
     var pair = paired[i];
     if(pair.value != ' ') {
@@ -64,10 +71,57 @@ console.log(paired);
       encryptedMessage += alphabet[result];
     }
   }
+  res.locals.encryptedMessage = encryptedMessage;
+  res.locals.decryptedMessage = decryptedMessage;
+  res.render('home');
+});
 
-  res.send({
-    encryptedValue: encryptedMessage
-  });
+// Read input from user and decrypt message using provided key and rot type
+app.post('/decrypt', function(req, res, next) {
+
+  var paired = new Array();
+  var keyLength = req.body.decryptKey.length;
+  var count = 0;
+  for (var i = 0; i < req.body.decryptValue.replace(/\s/g, '').length; i++) {
+    if (count >= keyLength) {
+      count = 0;
+    }
+    paired.push({
+      value: req.body.decryptValue.replace(/\s/g, '').charAt(i),
+      key: req.body.decryptKey.charAt(count)
+    });
+    count++;
+  }
+
+  for (var i = 0; i < paired.length; i++) {
+    var pair = paired[i];
+    if(pair.value != ' ') {
+      var rot;
+      var value;
+      for (var j = 0; j < alphabet.length; j++) {
+          if (alphabet[j] == pair.key) {
+            rot = j;
+          }
+          if (alphabet[j] == pair.value) {
+            value = j;
+          }
+      }
+    }
+    var result = value - rot;
+    if(req.body.decryptType == "rot1") {
+      result = result - 1;
+    }
+
+    if (result < 0) {
+      result = result + alphabet.length;
+    }
+    if(result >= 0) {
+      decryptedMessage += alphabet[result];
+    }
+  }
+  res.locals.decryptedMessage = decryptedMessage;
+  res.locals.encryptedMessage = encryptedMessage;
+  res.render('home');
 });
 
 var port = process.env.PORT || 3000; //can run both on Azure or local
